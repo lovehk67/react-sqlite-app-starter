c635bf5c5f4186530bad4dab3fa6c8eccc8b462e
park hye kyoung <hkpark@kfems.kr>
2024-05-23 14:52:23 +0900

    uninstall jeep-sqlite

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { defineCustomElements as jeepSqlite, applyPolyfills, JSX as LocalJSX  } from "jeep-sqlite/loader";
-import { HTMLAttributes } from 'react';
 import { Capacitor } from '@capacitor/core';
 import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite';
 
-type StencilToReact<T> = {
-  [P in keyof T]?: T[P] & Omit<HTMLAttributes<Element>, 'className'> & {
-    class?: string;
-  };
-} ;
-
-declare global {
-  export namespace JSX {
-    interface IntrinsicElements extends StencilToReact<LocalJSX.IntrinsicElements> {
-    }
-  }
-}
-
-applyPolyfills().then(() => {
-  jeepSqlite(window);
-});
 window.addEventListener('DOMContentLoaded', async () => {
   console.log('$$$ in index $$$');
   const platform = Capacitor.getPlatform();
   const sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite)
   try {
-    if(platform === "web") {
-      const jeepEl = document.createElement("jeep-sqlite");
-      document.body.appendChild(jeepEl);
-      await customElements.whenDefined('jeep-sqlite');
-      await sqlite.initWebStore();
-    }
     const ret = await sqlite.checkConnectionsConsistency();
     const isConn = (await sqlite.isConnection("db_issue9", false)).result;
     var db: SQLiteDBConnection
