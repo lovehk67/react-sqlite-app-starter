25320cf3a7f374492b3830d20831e979781d4aaf
park hye kyoung <hkpark@kfems.kr>
2024-05-23 13:47:50 +0900

    remove service-worker, reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import { defineCustomElements as jeepSqlite, applyPolyfills, JSX as LocalJSX  } from "jeep-sqlite/loader";
 import { HTMLAttributes } from 'react';
 import { Capacitor } from '@capacitor/core';
@@ -63,16 +61,6 @@ window.addEventListener('DOMContentLoaded', async () => {
         <App />
       </React.StrictMode>
     );
-    
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: https://cra.link/PWA
-    serviceWorkerRegistration.unregister();
-    
-    // If you want to start measuring performance in your app, pass a function
-    // to log results (for example: reportWebVitals(console.log))
-    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-    reportWebVitals();
   } catch (err) {
     console.log(`Error: ${err}`);
     throw new Error(`Error: ${err}`)
